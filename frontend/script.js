1fa54bb162f4390a5ec9bb78de7e1abb33f3a19c
Mohammed al Mubarish <aibusiness0630@gmail.com>
2026-07-16 18:04:49 +0530

    Day 5 Product List and Delete Button

--- frontend/script.js
+++ frontend/script.js
@@ -4,6 +4,13 @@ function addProduct() {
     let quantity = Number(document.getElementById("quantity").value);
     total = total + (price * quantity);
     document.getElementById("total").innerText = "Total : ₹" + total;
+    let list = document.getElementById("billList");
+let item = document.createElement("li");
+item.innerHTML =
+document.getElementById("product").value +
+" - ₹" + (price * quantity) +
+' <button onclick="this.parentElement.remove()">❌</button>';
+list.appendChild(item);
     document.getElementById("product").value = "";
     document.getElementById("price").value = "";
     document.getElementById("quantity").value = "";

39b314543e26cdf8f174c98601a9aedd77db4916
Mohammed al Mubarish <aibusiness0630@gmail.com>
2026-07-16 18:32:38 +0530

    Day 6 Complete

--- frontend/script.js
+++ frontend/script.js
@@ -1,17 +1,29 @@
 let total = 0;
+let products =[];
 function addProduct() {
     let price = Number(document.getElementById("price").value);
     let quantity = Number(document.getElementById("quantity").value);
     total = total + (price * quantity);
+    products.push({
+    name: document.getElementById("product").value,
+    price: price,
+    quantity: quantity
+});
     document.getElementById("total").innerText = "Total : ₹" + total;
     let list = document.getElementById("billList");
 let item = document.createElement("li");
+let amount = price * quantity;
 item.innerHTML =
 document.getElementById("product").value +
-" - ₹" + (price * quantity) +
-' <button onclick="this.parentElement.remove()">❌</button>';
+" - ₹" + amount +
+` <button onclick="deleteItem(this, ${amount})">❌</button>`;
 list.appendChild(item);
     document.getElementById("product").value = "";
     document.getElementById("price").value = "";
     document.getElementById("quantity").value = "";
+}
+function deleteItem(button, amount) {
+    total = total - amount;
+    document.getElementById("total").innerText = "Total : ₹" + total;
+    button.parentElement.remove();
 }

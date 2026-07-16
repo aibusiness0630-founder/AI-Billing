e8d121fd61a485b5bdafcde1e64626e983a46d6a
Mohammed al Mubarish <aibusiness0630@gmail.com>
2026-07-16 17:46:16 +0530

    Day 4 Total Calculation

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,10 @@
-alert("Welcome to AI Billing");
+let total = 0;
+function addProduct() {
+    let price = Number(document.getElementById("price").value);
+    let quantity = Number(document.getElementById("quantity").value);
+    total = total + (price * quantity);
+    document.getElementById("total").innerText = "Total : ₹" + total;
+    document.getElementById("product").value = "";
+    document.getElementById("price").value = "";
+    document.getElementById("quantity").value = "";
+}

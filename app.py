d0503a433a9f392c50cc953001a2a9e3706fd1f6
Mohammed al Mubarish <aibusiness0630@gmail.com>
2026-07-19 00:19:18 +0530

    Day 12: Added SQLite database and started Bill History

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///billing.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+class Bill(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_name = db.Column(db.String(100))
+    mobile = db.Column(db.String(20))
+    product = db.Column(db.String(100))
+    quantity = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    total = db.Column(db.Float)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -11,8 +24,19 @@ def home():
 
         quantity = int(request.form.get("quantity"))
         price = float(request.form.get("price"))
-
         total = quantity * price
+
+        new_bill = Bill(
+            customer_name=customer_name,
+            mobile=mobile,
+            product=product,
+            quantity=quantity,
+            price=price,
+            total=total
+        )
+
+        db.session.add(new_bill)
+        db.session.commit()
 
         return f"""
         <h2>Bill</h2>
@@ -23,8 +47,15 @@ def home():
         Price: ₹{price}<br>
         <h3>Total: ₹{total}</h3>
         """
+       
 
     return render_template("index.html")
+@app.route("/history")
+def history():
+    bills = Bill.query.all()
+    return render_template("history.html", bills=bills)
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
